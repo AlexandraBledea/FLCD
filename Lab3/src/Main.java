4b071e08082f778b3d2d4b398dbcac221c0e4ca0
Bledea Mihaela Alexandra <bledea_alexandra@yahoo.com>
2022-10-31 16:51:26 +0200

    Lab3

---
+++ Lab3/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
+public class Main {
+    private static void printToFile(String filePath, Object object) {
+        try(PrintStream printStream = new PrintStream(filePath)) {
+            printStream.println(object);
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void run(String filePath) {
+        MyScanner scanner = new MyScanner(filePath);
+        scanner.scan();
+        printToFile(filePath.replace(".txt", "ST.txt"), scanner.getSymbolTable());
+        printToFile(filePath.replace(".txt", "PIF.txt"), scanner.getPif());
+    }
+
+    public static void main(String[] args) {
+        run("IO/p1.txt");
+    }
+}
