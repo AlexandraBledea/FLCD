7022894d1d9c7a6e7c5c677c5f972fabfc788465
Bledea Mihaela Alexandra <bledea_alexandra@yahoo.com>
2022-11-02 08:16:15 +0200

    Added documentation for the HashTable class

--- Lab3/src/HashTable.java
+++ Lab3/src/HashTable.java
@@ -13,6 +13,11 @@ public class HashTable {
         }
     }
 
+    /**
+     * With this method we return an element based on its position
+     * @param pos - represents the position of the element from the hashtable
+     * @return - returns the elements
+     */
     public String findByPos(Pair<Integer, Integer> pos){
         if(this.table.size() <= pos.getFirst() || this.table.get(pos.getFirst()).size() <= pos.getSecond()){
             throw new IndexOutOfBoundsException("Invalid position");
@@ -21,17 +26,26 @@ public class HashTable {
         return this.table.get(pos.getFirst()).get(pos.getSecond());
     }
 
+    /**
+     * This method returns the size of the haspmap
+     * @return the size of the hashmap
+     */
     public Integer getSize(){
         return size;
     }
 
-    public Pair<Integer, Integer> findPositionOfTerm(String term){
-        int pos = hash(term);
+    /**
+     * With this method we look for the position of an element and return it
+     * @param elem - the element for which we want to find the position
+     * @return - the position of the element
+     */
+    public Pair<Integer, Integer> findPositionOfTerm(String elem){
+        int pos = hash(elem);
 
         if(!table.get(pos).isEmpty()){
             ArrayList<String> elems = this.table.get(pos);
             for(int i = 0; i < elems.size(); i++){
-                if(elems.get(i).equals(term)){
+                if(elems.get(i).equals(elem)){
                     return new Pair<>(pos, i);
                 }
             }
@@ -40,6 +54,11 @@ public class HashTable {
         return null;
     }
 
+    /**
+     * This method represents the hash function (used to compute the hash for an element)
+     * @param key - the element for which we compute the hash
+     * @return - the hash of the element
+     */
     private Integer hash(String key){
         int sum_chars = 0;
         char[] key_characters = key.toCharArray();
@@ -49,19 +68,29 @@ public class HashTable {
         return sum_chars % size;
     }
 
-    public boolean containsTerm(String term){
-        return this.findPositionOfTerm(term) != null;
+    /**
+     * This method looks whether the hashmap contains a specific element
+     * @param elem - the element we are looking for in the hashmap
+     * @return - returns true if the element is in the hashmap and false otherwise
+     */
+    public boolean containsTerm(String elem){
+        return this.findPositionOfTerm(elem) != null;
     }
 
-    public boolean add(String term){
-        if(containsTerm(term)){
+    /**
+     * This method adds a new element in the hashmap
+     * @param elem - the element we want to add in the hashmap
+     * @return - return true if the element was added successfully in the hashmap and false if the element was already in the hashmap
+     */
+    public boolean add(String elem){
+        if(containsTerm(elem)){
             return false;
         }
 
-        Integer pos = hash(term);
+        Integer pos = hash(elem);
 
         ArrayList<String> elems = this.table.get(pos);
-        elems.add(term);
+        elems.add(elem);
 
         return true;
     }
